933d2ef42a7f2bd81b8310decf57a0d03710a289
Ryker Cutler <rykercutler@gmail.com>
2023-03-18 07:49:33 -0600

    A simple webpage

---
+++ assignments-3/javascript.js
@@ -0,0 +1,5 @@
+const signInElement = document.querySelector(".SignIn")
+signInElement.addEventListener("click", function(){
+    console.log("working?")
+    window.location.href = "fourth-page.html"
+});
